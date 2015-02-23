a8a85c503ad0da3432042a9030c3ddfb370d7218
Jeremy A Kansas <ymerejsasnak@gmail.com>
2015-02-22 21:52:12 -0500

    basic display

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # mastermind
 Mastermind game in JS (etc)
+
+
+for practice using jquery UI and trying really hard to write clean/tight js
+also trying to design mobile first and make it responsive
---
+++ mm.js
@@ -0,0 +1,23 @@
+"use strict";
+
+
+
+$(function() {
+  
+  var game = {
+    colors: ["red", "blue", "green", "yellow", "brown", "pink"],
+    pegs: ["black", "white"]
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+})
