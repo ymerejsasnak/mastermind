b6dcfe18f34fa2747db65d383c668bfd0bab5204
Jeremy A Kansas <ymerejsasnak@gmail.com>
2015-02-23 21:44:59 -0500

    basic UI functionality done

--- mm.js
+++ mm.js
@@ -6,8 +6,35 @@ $(function() {
   
   var game = {
     colors: ["red", "blue", "green", "yellow", "brown", "pink"],
-    pegs: ["black", "white"]
+    pegs: ["black", "white"],
+    currentRow: 1,
+    currentPeg: 0
   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  $(".color-select").on("click", function() {
+    var color = $(this).attr("class");
+
+    $("#row" + game.currentRow + " .hole:nth-child(" + (game.currentPeg + 2) + ")").removeClass().addClass("hole " + color);
+    game.currentPeg = (game.currentPeg + 1) % 4;
+
+    if (game.currentPeg === 0) {
+      game.currentRow++;  //temp...call row check function here and put in white/black pegs
+    }
+
+  })
 
 
 
