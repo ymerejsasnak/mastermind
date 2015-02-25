512083ee9a5b1f33b4a1dddf35b68f9bea2973b2
Jeremy A Kansas <ymerejsasnak@gmail.com>
2015-02-24 23:41:40 -0500

    working on verifying guess and determining hint pegs

--- mm.js
+++ mm.js
@@ -7,10 +7,13 @@ $(function() {
 
   function Game() {
     this.colors = ["red", "blue", "green", "yellow", "brown", "pink"];
-    this.pegs = ["black", "white"];
+    this.hintPegs = {black: 0, white: 0};
+
     this.currentRow = 1;
     this.currentPeg = 0;
+
     this.sequence = [];
+    this.guess = [];
   }
 
   Game.prototype.createSequence = function() {
@@ -19,30 +22,54 @@ $(function() {
                     this.colors[Math.floor(Math.random() * 6)], 
                     this.colors[Math.floor(Math.random() * 6)], 
                     this.colors[Math.floor(Math.random() * 6)]]
-  }
+  };
 
   Game.prototype.setSequence = function() {
     //set colors from sequence to answer divs in game display
     this.sequence.forEach(function(color, index) {
       $("#row-solution .hole:nth-child(" + (index + 2) + ")").addClass(color);  
     })
-    
+  };
 
+  Game.prototype.checkGuess = function() {
+    
+    //working on this...maybe instead need to put both arrays into seperate arrays that I can delete from as I check
+
+    //detect correct color and placement
+    this.guess.forEach(function(guessCol, guessIndex) {
+      if (guessCol === this.sequence[guessIndex]) {
+        this.hintPegs.black++;        
+      }
+      
+
+    }.bind(this));
+
+    
+    console.log(this.hintPegs)
   }
 
+  Game.prototype.showHints = function() {
 
+  }
 
   
   
   //handler for selection of color guesses
-  $(".color-select").on("click", function() {
+  $("#colors div").on("click", function() {
     var color = $(this).attr("class");
+
+    game.guess.push(color);
 
     $("#row" + game.currentRow + " .hole:nth-child(" + (game.currentPeg + 2) + ")").removeClass().addClass("hole " + color);
     game.currentPeg = (game.currentPeg + 1) % 4;
 
     if (game.currentPeg === 0) {
-      game.currentRow++;  //temp...call row check function here and put in white/black pegs
+      game.checkGuess();
+      game.showHints();
+      game.currentRow++;
+      //reset guesses and hints
+      game.guess = [];
+      game.hintPegs = {black: 0, white: 0}
     }
 
   })
