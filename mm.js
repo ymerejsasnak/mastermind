3b5933d50238dd3238aae420108dcbed7f01fb8d
Jeremy A Kansas <ymerejsasnak@gmail.com>
2015-02-24 22:47:59 -0500

    sequence is generated set in display

--- mm.js
+++ mm.js
@@ -4,26 +4,37 @@
 
 $(function() {
   
-  var game = {
-    colors: ["red", "blue", "green", "yellow", "brown", "pink"],
-    pegs: ["black", "white"],
-    currentRow: 1,
-    currentPeg: 0
+
+  function Game() {
+    this.colors = ["red", "blue", "green", "yellow", "brown", "pink"];
+    this.pegs = ["black", "white"];
+    this.currentRow = 1;
+    this.currentPeg = 0;
+    this.sequence = [];
+  }
+
+  Game.prototype.createSequence = function() {
+    //get 4 random colors from set of colors
+    this.sequence =[this.colors[Math.floor(Math.random() * 6)], 
+                    this.colors[Math.floor(Math.random() * 6)], 
+                    this.colors[Math.floor(Math.random() * 6)], 
+                    this.colors[Math.floor(Math.random() * 6)]]
+  }
+
+  Game.prototype.setSequence = function() {
+    //set colors from sequence to answer divs in game display
+    this.sequence.forEach(function(color, index) {
+      $("#row-solution .hole:nth-child(" + (index + 2) + ")").addClass(color);  
+    })
+    
+
   }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
+  
+  
+  //handler for selection of color guesses
   $(".color-select").on("click", function() {
     var color = $(this).attr("class");
 
@@ -44,7 +55,10 @@ $(function() {
 
 
 
+  var game = new Game;
 
+  game.createSequence()
+  game.setSequence()
 
 
 })
